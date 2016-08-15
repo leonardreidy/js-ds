d1804a34712f85e80666c5bc5c86982f8bbba7ca
Leonard M Reidy <leonard.reidy@gmail.com>
2016-08-15 15:19:55 +0100

    Refactor Stack Prototype as Object Literal of Stack Methods

--- src/Queue.js
+++ src/Queue.js
@@ -30,112 +30,116 @@ var Queue = (function () {
         this.offset = 0;
     }
 
-    /**
-     * @memberOf  Queue
-     * @instance
-     * @method  getLength
-     * @description  Get the length of the queue.
-     * @return {number} Return the length/number of elements in the queue
-     */
-    Queue.prototype.getLength = function () {
-        return (this.queue.length - this.offset);
-    };
+    Queue.prototype = {
 
-    /**
-     * @memberOf  Queue
-     * @instance
-     * @method  isEmpty
-     * @description  Determine whether the queue is empty or not.
-     * @return {Boolean} Return true if queue is empty, false if not
-     */
-    Queue.prototype.isEmpty = function () {
-        return (this.queue.length === 0);
-    };
+        /**
+         * @memberOf  Queue
+         * @instance
+         * @method  getLength
+         * @description  Get the length of the queue.
+         * @return {number} Return the length/number of elements in the queue
+         */
+        getLength: function () {
+            return (this.queue.length - this.offset);
+        },
 
-    /**
-     * @memberOf  Queue
-     * @instance
-     * @method  enqueue
-     * @description  Add an element to the back of the queue.
-     * @param  {*} element Any data type
-     */
-    Queue.prototype.enqueue = function (element) {
-        this.queue.push(element);
-    };
+        /**
+         * @memberOf  Queue
+         * @instance
+         * @method  isEmpty
+         * @description  Determine whether the queue is empty or not.
+         * @return {Boolean} Return true if queue is empty, false if not
+         */
+        isEmpty: function () {
+            return (this.queue.length === 0);
+        },
 
-    /**
-     * @memberOf  Queue
-     * @instance
-     * @method  dequeue
-     * @description  Remove an element from the front of the queue.
-     * @return {*} Return the dequeued element, or -1 if the queue is empty
-     */
-    Queue.prototype.dequeue = function () {
-        if (this.queue.length === 0) {
-        	return undefined;
-        }
-        var element = this.queue.shift();
-        return element;
-    };
+        /**
+         * @memberOf  Queue
+         * @instance
+         * @method  enqueue
+         * @description  Add an element to the back of the queue.
+         * @param  {*} element Any data type
+         */
+        enqueue:function (element) {
+            this.queue.push(element);
+        },
 
-    // Clear/reinitialise the queue
-    
-    // Exercise some caution with delete - it has limitations and 
-    // constraints. In general, if you try to delete a property that
-    // does not exist, the delete operation will return true, which 
-    // may be unexpected. Moreover, if the delete operator succeeds, it
-    // removes the property from the object entirely but if a property 
-    // with the same name exists on the object's prototype chain, the object
-    // will inherit that property from the prototype, another potentially
-    // unintuitive outcome with real potential for driving buggy behaviour
-    // in an application.
-    
-    // See Mozilla Developer Network (MDN) for more info:
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
-    
-    /**
-     * @memberOf  Queue
-     * @instance
-     * @method  clear
-     * @description  Clear the queue.
-     * 
-     */
-    Queue.prototype.clear = function() {
-        delete this.queue;
-    	this.queue = [];
-    };
+        /**
+         * @memberOf  Queue
+         * @instance
+         * @method  dequeue
+         * @description  Remove an element from the front of the queue.
+         * @return {*} Return the dequeued element, or -1 if the queue is empty
+         */
+        dequeue: function () {
+            if (this.queue.length === 0) {
+                return undefined;
+            }
+            var element = this.queue.shift();
+            return element;
+        },
 
-    /**
-     * @memberOf  Queue
-     * @instance
-     * @method  front
-     * @description Get the element at the front of the queue without dequeueing it.
-     * @return {*} Return the element at the front of the queue, or undefined if the queue is empty
-     */
-    Queue.prototype.front = function () {
-        return (this.queue.length > 0 ? this.queue[this.offset] : undefined);
-    };
+        // Clear/reinitialise the queue
+        
+        // Exercise some caution with delete - it has limitations and 
+        // constraints. In general, if you try to delete a property that
+        // does not exist, the delete operation will return true, which 
+        // may be unexpected. Moreover, if the delete operator succeeds, it
+        // removes the property from the object entirely but if a property 
+        // with the same name exists on the object's prototype chain, the object
+        // will inherit that property from the prototype, another potentially
+        // unintuitive outcome with real potential for driving buggy behaviour
+        // in an application.
+        
+        // See Mozilla Developer Network (MDN) for more info:
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
+        
+        /**
+         * @memberOf  Queue
+         * @instance
+         * @method  clear
+         * @description  Clear the queue.
+         * 
+         */
+        clear: function() {
+            delete this.queue;
+            this.queue = [];
+        },
 
-    /**
-     * @memberOf  Queue
-     * @instance
-     * @method  back
-     * @description  Get the element at the back of the queue without dequeueing it.
-     * @return {*} Return the element at the back of the queue, or undefined if the queue is empty
-     */
-    Queue.prototype.back = function() {
-    	return(this.queue.length > 0 ? this.queue[this.queue.length-1] : undefined);
-    };
+        /**
+         * @memberOf  Queue
+         * @instance
+         * @method  front
+         * @description Get the element at the front of the queue without dequeueing it.
+         * @return {*} Return the element at the front of the queue, or undefined if the queue is empty
+         */
+        front: function () {
+            return (this.queue.length > 0 ? this.queue[this.offset] : undefined);
+        },
 
-    /**
-     * @memberOf  Queue
-     * @instance
-     * @method  toString
-     * @description  Get a string representation of the queue.
-     * @return {string} Return string representation of the queue (using Object.prototype.toString())
-     */
-    Queue.prototype.toString = function() {
-        return this.queue.toString();
+        /**
+         * @memberOf  Queue
+         * @instance
+         * @method  back
+         * @description  Get the element at the back of the queue without dequeueing it.
+         * @return {*} Return the element at the back of the queue, or undefined if the queue is empty
+         */
+        back: function() {
+            return(this.queue.length > 0 ? this.queue[this.queue.length-1] : undefined);
+        }, 
+
+        /**
+         * @memberOf  Queue
+         * @instance
+         * @method  toString
+         * @description  Get a string representation of the queue.
+         * @return {string} Return string representation of the queue (using Object.prototype.toString())
+         */
+        toString: function() {
+            return this.queue.toString();
+        }, 
+
     };
 
     return Queue;
